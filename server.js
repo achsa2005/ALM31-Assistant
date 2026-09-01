fbe28c0af4cc72a7b0bd7cbcf0f06255b811facf
ACHSA JOSE <achsajose2005@gmail.com>
2026-09-01 14:26:56 +0530

    Update server

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ const VIDEO_MAP = [
         ]
     },
     {
-        file: "videos/INDUVIDUAL_HARMONICS__mp4.mp4",
+        file: "videos/INDUVIDUAL_HARMONICS__.mp4",
         keywords: [
             "harmonics", "individual harmonics", "harmonic analysis", "harmonic distortion",
             "harmonic content", "harmonic measurement", "harmonic voltage", "harmonic current",
