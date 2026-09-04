454584e13cbc347f3235695f1fdaa5a0a742267b
ACHSA JOSE <achsajose2005@gmail.com>
2026-09-04 12:23:13 +0530

    Show date time images vertically

--- server.js
+++ server.js
@@ -612,28 +612,24 @@ const STEP_IMAGE_SEQUENCES = [
       "how do i set the time"
     ],
 
-    steps: [
-      {
+   steps: [
+    {
         image: "images/Configuration_button.png",
         caption: "Press the Configuration button, which is the gear icon on the device."
-      },
-      {
-        image: "images/Date_and_Time_inside_button.png",
-        caption: "Select the Date Time option from the menu."
-      },
-      {
+    },
+    {
         image: "images/Date_and_time_display.png",
         caption: "Use the left and right arrow buttons to move between the date and time fields."
-      },
-      {
-        image: "images/Date_and_time_display.png",
-        caption: "Use the up and down arrow buttons to adjust the date, hours, and minutes."
-      },
-      {
+    },
+    {
+        image: "images/Date_and_Time_inside_button.png",
+        caption: "Select the Date Time option from the menu."
+    },
+    {
         image: "images/Date_and_Time_in_brief.png",
         caption: "Press ENTER to save your settings."
-      }
-    ]
+    }
+]
   }
 ];
 
